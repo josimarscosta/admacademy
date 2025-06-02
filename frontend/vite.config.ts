4b600f04f601feb354f949fc881c40eee02ab0d5
josimarscosta <64698292+josimarscosta@users.noreply.github.com>
2025-06-01 21:20:34 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  root: __dirname, // Diretório raiz do projeto
+  build: {
+    outDir: 'dist',
   },
 })
-
